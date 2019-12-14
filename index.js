cf7a2885b3e9958fcc7e9f4732e881d63fd3086b
Yining Wang <yining.wang@utexas.edu>
2019-12-14 14:54:49 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    let data = {name: name, email: email};
+
+    let object = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+
+    let body = document.querySelector('body');
+
+    return fetch("http://localhost:3000/users", object)
+        .then(response => response.json())
+        .then(function(result) {
+            console.log(result.id)
+            body.textContent = result.id;
+        }).catch(function(error) {
+            body.textContent += "Unauthorized Access";
+            alert("Unauthorized Access");
+            console.log(error.message);
+        });
+}
